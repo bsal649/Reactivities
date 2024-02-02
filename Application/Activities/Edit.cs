9eef33d8e26aa321ccbba3152169581714e7b1d0
bsal649 <ben.salmon@shape.co.nz>
2024-02-02 16:19:02 +1300

    End of section 4

---
+++ Application/Activities/Edit.cs
@@ -0,0 +1,30 @@
+using AutoMapper;
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Edit
+    {
+        public class Command : IRequest
+        {
+            public Activity Activity { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+            public async Task Handle(Command request, CancellationToken cancellationToken)
+            {
+                _context.Activities.Update(request.Activity);
+
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
