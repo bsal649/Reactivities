747c044354a11f2686c16f035084cce66c6eb1b2
bsal649 <ben.salmon@shape.co.nz>
2024-04-15 16:36:33 +1200

    fix eslint config

--- client-app/.eslintrc.cjs
+++ client-app/.eslintrc.cjs
@@ -15,6 +15,6 @@ module.exports = {
       { allowConstantExport: true },
     ],
     'no-unused-vars': 'warn',
-    '@typescript-eslint/no-unused-vars': 'warning',
+    '@typescript-eslint/no-unused-vars': 'warn',
   },
 };
